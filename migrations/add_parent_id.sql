9f8176fc721a0e9905425340dfb5257112432e3a
小星虎 <105782492+xingxinag@users.noreply.github.com>
2025-09-19 19:15:34 +0800

    Delete migrations directory

--- migrations/add_parent_id.sql
+++
@@ -1,6 +0,0 @@
--- 添加parent_id字段到category表中
-ALTER TABLE category ADD COLUMN parent_id INTEGER;
-
--- 添加外键约束
-ALTER TABLE category ADD CONSTRAINT fk_category_parent 
-    FOREIGN KEY (parent_id) REFERENCES category(id); 
