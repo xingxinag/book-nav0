af4f5da2b6f3da0dc6c004e6961428d89c5cd184
小星虎 <105782492+xingxinag@users.noreply.github.com>
2025-09-19 19:28:28 +0800

    Add files via upload

---
+++ app/auth/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from app.models import User, InvitationCode
+
+class LoginForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('登录')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField('密码', validators=[DataRequired(), Length(min=6)])
+    password2 = PasswordField('确认密码', validators=[DataRequired(), EqualTo('password')])
+    invitation_code = StringField('邀请码', validators=[DataRequired()])
+    submit = SubmitField('注册')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('该用户名已被使用，请选择其他用户名')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('该邮箱已被注册，请使用其他邮箱')
+    
+    def validate_invitation_code(self, invitation_code):
+        code = InvitationCode.query.filter_by(code=invitation_code.data, is_active=True).first()
+        if code is None or code.used_by_id is not None:
+            raise ValidationError('无效的邀请码') 
